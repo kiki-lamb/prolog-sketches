d91812bee4cedcdd18f80cb461ccdf9948f46924
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-28 10:45:47 -0400

    wip

--- graph.pl
+++ graph.pl
@@ -14,18 +14,17 @@ path(Here, Move, To, Path)                        :-
     reverse([To|TmpPath], Path).
     
 search(Here, Move, To, Path, Build)               :-
-    found(Here, To, Path, Build);
+    found(Here, To, stop, Path, Build);
     descend(Here, Move, To, Path, [Here|Build]).
 
 descend(Here, Move, To, Path, Build) :-
-    F =.. [Move, Here, Next],
-    call(F),
+    G =.. [Move, Here, Next],call(G),
     not(member(Next,Build)),
     search(Next, Move, To, Path, Build).
 
-found(Here, Here, Path, Build) :-
+found(Here, Here, _, Path, Build) :-
     Path = Build. 
 
-found(Here, To, Path, Build) :-
-    stop(Here, To),
-    found(Here, Here, Path, [Here|Build]).
+found(Here, To, Stop, Path, Build) :-
+    G =.. [Stop, Here, To], call(G),
+    found(Here, Here, Stop, Path, [Here|Build]).
