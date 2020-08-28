59e69917b068db64aa74f8a19ee47960fc5290c8
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-28 04:53:12 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-traverse(From, To):-
+move(From, To):-
     ((human(From), appliance(To));
      (person(From), ((person(To), (like(From,To);
                                    like(To,From)));
@@ -8,23 +8,30 @@ traverse(From, To):-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+stop(From, To) :-
+    From == To;
+    has(From, To).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 typed_path(Type, From, To)              :- typed_path(Type, From, To, _).
+
 typed_path(Type, From, To, Path)        :-
     type(From, Type),
     path(From, To, Path).
 
 path(From, To)                          :- path(From, To, _).
+
 path(X, [], Path)                       :- entity(X), Path = [].
+
 path(From, To, Path)                    :-
     path(From, To, TmpPath, []),
     reverse(TmpPath, TmpPath2),
     append(TmpPath2, [To], Path).
-path(From, To, Path, Build)             :-
-    (has(From,  To) ; From == To),
-    Path = [From|Build];
 
-    traverse(From, Next),
-    not(member(Next,Build)),
-    path(Next, To, Path, [From|Build]).
+path(From, To, Path, Build)             :-
+    stop(From, To),
+    Path = [From|Build];
+    (move(From, Next),
+     not(member(Next,Build)),
+     path(Next, To, Path, [From|Build])).
