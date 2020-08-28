b5952912832ed711b5753e1a4d1f934bffe3351c
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-27 23:08:29 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -1,3 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+could(   Person, Action, Thing      )   :- could(Person, Action, Thing, _).
+could(   Person, Action, Thing, Path)   :-
+    a(   Person, Action, Thing      ),
+    path(Person, Thing,  Path       ).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+a(Appliance, break,  [])   :- appliance(Appliance), \+ could(    _,      repair, Appliance   ).
+a(Person,    starve, [])   :- person(   Person   ), \+ could(    Person, eat,    _           ).
+a(Person,    chase,  X)    :- cat(      Person   ),    cat(      X),     chase(  Person, X   ).
+a(Person,    drink,  X)    :- person(   Person   ),    thing(    X),     drink(  Person, X   ).
+a(Person,    eat,    X)    :- person(   Person   ),    thing(    X),     eat(    Person, X   ).
+a(Person,    repair, X)    :- human(    Person   ),    appliance(X),     path(   Person, nail).
+a(Person,    shop,   X)    :- person(   Person   ),    store(    X),     shop(   Person, X   ).
+a(Person,    smoke,  X)    :- person(   Person   ),    thing(    X),     smoke(  Person, X   ).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 path(From, To)                          :- path(From, To, _).
 path(_, [], Path)                       :- Path = [].
 path(From, To, Path)                    :-
@@ -11,10 +31,13 @@ path(From, To, Path, Build)             :-
 path(From, From, Build, Path)           :-
     Path = Build.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 traverse(From, To):-
     (human(From),  appliance(To));
     (person(From), person(To), like(From,To));
     (person(From), person(To), like(To,From));
     (person(From), store(To),  shop(From,To));
     (thing(To),    has(From, To)).
+
 
--- main.pl
+++ main.pl
@@ -2,20 +2,6 @@
 :- consult("facts.pl").
 :- consult("graph.pl").
  
-a(Appliance, break,  [])   :- appliance(Appliance), \+ could(    _,      repair, Appliance).
-a(Person,    starve, [])   :- person(   Person   ), \+ could(    Person, eat,    _).
-a(Person,    repair, X)    :- human(    Person   ),    appliance(X),     path(   Person, nail).
-a(Person,    eat,    X)    :- person(   Person   ),    thing(    X),     eat(    Person, X).
-a(Person,    drink,  X)    :- person(   Person   ),    thing(    X),     drink(  Person, X).
-a(Person,    smoke,  X)    :- person(   Person   ),    thing(    X),     smoke(  Person, X).
-a(Person,    shop,   X)    :- person(   Person   ),    store(    X),     shop(   Person, X).
-a(Person,    chase,  X)    :- cat(      Person   ),    cat(      X),     chase(  Person, X).
-
-could(   Person, Action, Thing      )   :- could(Person, Action, Thing, _).
-could(   Person, Action, Thing, Path)   :-
-    a(   Person, Action, Thing      ),
-    path(Person, Thing,  Path       ).
-
 loop :-
     could(W,A,T,P),
     format("~w could ~w ~w: ~w.\n", [W, A, T, P]),
