a46da3e09b97ceec7d7bf3b431b76f802017ce53
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-29 01:34:27 -0400

    petting

--- exists.pl
+++ exists.pl
@@ -1,11 +1,38 @@
+thing(nail).
+thing(weed).
+thing(cigarette).
+
+beverage(energy_drink).
+beverage(pepsi).
+beverage(water).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+alcohol(beer).
+alcohol(tequila).
+alcohol(vodka).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+vegetable(cabbage).
+vegetable(turnip). 
+vegetable(potato). 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+meat(turkey).
+meat(cat_food).
+meat(pepperoni).
+meat(steak).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 person(sybil).
 person(kiki).
 person(higgy).
 person(boson).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cat(higgy).
 cat(boson).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 store(freshco).
 store(home_hardware).
 store(lcbo).
@@ -13,6 +40,7 @@ store(mr_meat).
 store(dave).
 store(chicken_freak).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 shop_at(kiki,  lcbo).
 shop_at(kiki,  chicken_freak).
 shop_at(kiki,  freshco).
@@ -20,9 +48,11 @@ shop_at(sybil, home_hardware).
 shop_at(sybil, mr_meat).
 shop_at(sybil, dave).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 like(sybil, kiki).
 like(kiki,  boson).
 like(higgy, boson).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 appliance(dish_washer).
 appliance(sink).
--- infer.pl
+++ infer.pl
@@ -1,53 +1,55 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start(X)                             :- person(X) ;
-                                        appliance(X).
+start(X)                               :- person(X) ;
+                                          appliance(X).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-move(Here, To)                       :- person(Here),
-                                        (\+ cat(Here),
-                                         (appliance(To)) ;
-                                         (person(To), (like(Here,To) ;
-                                                       like(To,Here))) ;
-                                         store(To), shop_at(Here,To)).
+move(Here, To)                         :- person(Here),
+                                          (\+ cat(Here),
+                                           (appliance(To)) ;
+                                           (person(To), (like(Here,To) ;
+                                                         like(To,Here))) ;
+                                           store(To), shop_at(Here,To)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-stop(Here, To)                       :- (store(Here) ;
-                                         appliance(Here)),
-                                        has(Here, To).
+stop(Here, To)                         :- (store(Here) ;
+                                           appliance(Here)),
+                                          has(Here, To).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-apath(Here, To)                      :- apath(Here, To, _).
-apath(Here, To, Path)                :- path(start, Here, move, To, stop, Path).
+apath(Here, To)                        :- apath(Here, To, _).
+apath(Here, To, Path)                  :- path(start, Here, move, To, stop, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-could(  Person, Action, Thing      ) :- could(Person, Action, Thing, _      ).
-could(  Person, Action, Thing, Path) :- would(Person, Action, Thing         ),
-                                        call((apath(Person,Thing,Path),   !)).
+could(  Person, Action, Thing      )   :- could(Person, Action, Thing, _      ).
+could(  Person, Action, Thing, Path)   :- would(Person, Action, Thing         ),
+                                          call((apath(Person,Thing,Path),   !)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-couldnt(Person, Action, Thing      ) :- couldnt(Person, Action, Thing, _    ).
-couldnt(Person, Action, Thing, Path) :- \+ could(Person, Action, Thing, Path).
+couldnt(Person, Action, Thing      )   :- couldnt(Person, Action, Thing, _    ).
+couldnt(Person, Action, Thing, Path)   :- \+ could(Person, Action, Thing, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-would(Person,    shower,  Person)    :- human(Person),
-                                        apath(Person, water).
-would(Person,    starve,  Person)    :- couldnt(Person, eat, _).
-would(Appliance, break,   Appliance) :- appliance(Appliance),
-                                        couldnt(_,  repair, Appliance).
-would(Person,    chase,   X)         :- cat(Person),
-                                        cat(X),
-                                        chase(Person, X).
-would(Person,    drink,   X)         :- thing(X),
-                                        drink(Person, X).
-would(Person,    eat,     X)         :- thing(X),
-                                        eat(Person, X).
-would(Person,    shop_at, X)         :- store(X),
-                                        shop_at(Person, X).
-would(Person,    smoke,   X)         :- thing(X),
-                                        smoke(Person, X).
-would(Person,    repair,  X)         :- human(Person),
-                                        appliance(X),
-                                        apath(Person, nail).
-would(Person,    pet,     X)         :- human(Person),
-                                        cat(X),                                        
-                                        (like(Person,X) ; like(X,Person)).
+would(Person,    shower,    Person)    :- human(Person),
+                                          apath(Person, water).
+would(Person,    starve,    Person)    :- couldnt(Person, eat, _).
+would(Appliance, break,     Appliance) :- appliance(Appliance),
+                                          couldnt(_,  repair, Appliance).
+would(Person,    chase,     X)         :- cat(Person),
+                                          cat(X).
+would(Person,    drink,     X)         :- object(X),
+                                          drink(Person, X).
+would(Person,    eat,       X)         :- thing(X),
+                                          eat(Person, X).
+would(Person,    shop_at,   X)         :- store(X),
+                                          shop_at(Person, X).
+would(Person,    smoke,     X)         :- thing(X),
+                                          smoke(Person, X).
+would(Person,    repair,    X)         :- human(Person),
+                                          appliance(X),
+                                          apath(Person, nail).
+would(Person,    pet,       X)         :- human(Person),
+                                          cat(X),                                        
+                                          (like(Person,X) ; like(X,Person)).
+would(Person,    scare_off, X)         :- human(Person),
+                                          cat(X),                                        
+                                          \+ (like(Person,X) ; like(X,Person)).
