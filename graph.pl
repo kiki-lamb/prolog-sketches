23b402cf8a9716aee96158a41220d195eaf397e7
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-29 00:09:09 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -1,15 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 path(   Start, Here, Move, To,   Stop      ) :- path(Start, Here, Move, To, Stop, _).
 path(   _,     _,    _,   [],    _,    Path) :- Path = [].
 path(   Start, Here, Move, To,   Stop, Path) :- call(Start, Here),
                                                 search(Here, Move, To, Stop, Tmp),
                                                 reverse([To|Tmp], Path).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 search(        Here, Move, To,   Stop, Path) :- search([], Here, Move, To, Stop, Tmp),
                                                 reverse([To|Tmp], Path).
 search( Build, Here, Move, To,   Stop, Path) :- found(Build, Here, Move, To, Stop, Path) ;
                                                 descend([Here|Build], Here, Move, To, Stop, Path).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 descend(Build, Here, Move, To,   Stop, Path) :- call(Move, Here, Next),
                                                 not(member(Next,Build)),
                                                 search(Build, Next, Move, To, Stop, Path).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 found(  Build, Here, _,    Here, _,    Path) :- Path = Build. 
 found(  Build, Here, _,    To,   Stop, Path) :- call(Stop, Here, To),
                                                 found([Here|Build], Here, _, Here, Stop, Path).
--- infer.pl
+++ infer.pl
@@ -1,48 +1,53 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start(X)               :- person(X).
+start(X)                             :- person(X).
 
-move(Here, To)         :- (person(Here),
-                           ((appliance(To)) ;
-                            (person(To), (like(Here,To) ;
-                                          like(To,Here))) ;
-                            store(To), shop(Here,To))).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+move(Here, To)                       :- person(Here),
+                                        ((appliance(To)) ;
+                                         (person(To), (like(Here,To) ;
+                                                       like(To,Here))) ;
+                                         store(To), shop(Here,To)).
 
-stop(Location, Target) :- (store(Location) ;
-                           appliance(Location)),
-                          has(Location, Target).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+stop(Here, Target)                   :- (store(Here) ;
+                                         appliance(Here)),
+                                        has(Here, Target).
 
-apath(Here, To)        :- apath(Here, To, _).
-apath(Here, To, Path)  :- path(start, Here, move, To, stop, Path).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+apath(Here, To)                      :- apath(Here, To, _).
+apath(Here, To, Path)                :- path(start, Here, move, To, stop, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 could(  Person, Action, Thing      ) :- could(Person, Action, Thing, _      ).
 could(  Person, Action, Thing, Path) :- a(    Person, Action, Thing         ),
                                         call((apath(Person,Thing,Path),   !)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 couldnt(Person, Action, Thing      ) :- couldnt(Person, Action, Thing, _    ).
 couldnt(Person, Action, Thing, Path) :- \+ could(Person, Action, Thing, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a(Person,    shower, []) :- human(Person),
-                            apath(Person, water).
-a(Person,    starve, []) :- person(Person),
-                            couldnt(Person, eat, _).
-a(Appliance, break,  []) :- appliance(Appliance),
-                            couldnt(_,  repair, Appliance).
-a(Person,    chase,  X)  :- cat(Person),
-                            cat(X),
-                            chase(Person, X).
-a(Person,    drink,  X)  :- person(Person),
-                            thing(X),
-                            drink(Person, X).
-a(Person,    eat,    X)  :- person(Person),
-                            thing(X),
-                            eat(Person, X).
-a(Person,    shop,   X)  :- person(Person),
-                            store(X),
-                            shop(Person, X).
-a(Person,    smoke,  X)  :- person(Person),
-                            thing(X),
-                            smoke(Person, X).
-a(Person,    repair, X)  :- human(Person),
-                            appliance(X),
-                            apath(Person, nail).
+a(Person,    shower, [])             :- human(Person),
+                                        apath(Person, water).
+a(Person,    starve, [])             :- person(Person),
+                                        couldnt(Person, eat, _).
+a(Appliance, break,  [])             :- appliance(Appliance),
+                                        couldnt(_,  repair, Appliance).
+a(Person,    chase,  X)              :- cat(Person),
+                                        cat(X),
+                                        chase(Person, X).
+a(Person,    drink,  X)              :- person(Person),
+                                        thing(X),
+                                        drink(Person, X).
+a(Person,    eat,    X)              :- person(Person),
+                                        thing(X),
+                                        eat(Person, X).
+a(Person,    shop,   X)              :- person(Person),
+                                        store(X),
+                                        shop(Person, X).
+a(Person,    smoke,  X)              :- person(Person),
+                                        thing(X),
+                                        smoke(Person, X).
+a(Person,    repair, X)              :- human(Person),
+                                        appliance(X),
+                                        apath(Person, nail).
