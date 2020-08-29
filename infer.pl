35df3ced510bc5079420195e7c97bfaee2bfc4be
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-28 22:43:44 -0400

    ...

--- infer.pl
+++ infer.pl
@@ -30,14 +30,14 @@ couldnt(   Person, Action, Thing, Path) :-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a(Person,    shower, [])   :- human(Person),           apath(    Person, water               ).
-a(Person,    starve, [])   :- person(   Person   ),  couldnt(    Person, eat,    _           ).
-a(Appliance, break,  [])   :- appliance(Appliance),  couldnt(    _,      repair, Appliance   ).
+a(Person,    shower, [])   :- human(    Person),    apath(    Person, water               ).
+a(Person,    starve, [])   :- person(   Person   ), couldnt(  Person, eat,    _           ).
+a(Appliance, break,  [])   :- appliance(Appliance), couldnt(  _,      repair, Appliance   ).
 
-a(Person,    chase,  X)    :- cat(      Person   ),    cat(      X),     chase(  Person, X   ).
-a(Person,    drink,  X)    :- person(   Person   ),    thing(    X),     drink(  Person, X   ).
-a(Person,    eat,    X)    :- person(   Person   ),    thing(    X),     eat(    Person, X   ).
-a(Person,    shop,   X)    :- person(   Person   ),    store(    X),     shop(   Person, X   ).
-a(Person,    smoke,  X)    :- person(   Person   ),    thing(    X),     smoke(  Person, X   ).
-a(Person,    repair, X)    :- human(    Person   ),    appliance(X),     apath(  Person, nail).
+a(Person,    chase,  X)    :- cat(      Person   ), cat(      X),     chase(  Person, X   ).
+a(Person,    drink,  X)    :- person(   Person   ), thing(    X),     drink(  Person, X   ).
+a(Person,    eat,    X)    :- person(   Person   ), thing(    X),     eat(    Person, X   ).
+a(Person,    shop,   X)    :- person(   Person   ), store(    X),     shop(   Person, X   ).
+a(Person,    smoke,  X)    :- person(   Person   ), thing(    X),     smoke(  Person, X   ).
+a(Person,    repair, X)    :- human(    Person   ), appliance(X),     apath(  Person, nail).
 
