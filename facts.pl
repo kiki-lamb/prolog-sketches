0169cfc2060e2c3915733b7a3a1e0abb4862bf87
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-28 22:32:40 -0400

    ...

--- facts.pl
+++ facts.pl
@@ -1,11 +1,6 @@
 chase(higgy, boson).
 chase(boson, higgy).
 
-beverage(X) :- alcohol(X).
-beverage(energy_drink).
-beverage(pepsi).
-beverage(water).
-
 alcohol(beer).
 alcohol(tequila).
 alcohol(vodka).
@@ -22,59 +17,78 @@ meat(steak).
 dislike(kiki, cat_food).
 dislike(kiki, tequila).
 
-thing(X) :- food(X); beverage(X); appliance(X).
+thing(X) :-
+    food(X) ;
+    beverage(X) ;
+    appliance(X).
+
 thing(nail).
 thing(weed).
 thing(cigarette).
 
-soft_drink(X)           :-
+soft_drink(X)         :-
     beverage(X), 
     \+ alcohol(X),
     X \= water.
 
-food(X)                 :- vegetable(X); meat(X).
+beverage(energy_drink).
+beverage(pepsi).
+beverage(water).
+beverage(X)           :- alcohol(X).
+food(X)               :- vegetable(X) ;
+                         meat(X).
+has(freshco, X)       :- vegetable(X).
+has(mr_meat, X)       :- meat(X).
+has(lcbo, X)          :- alcohol(X).
+has(chicken_freak, X) :- soft_drink(X).
+has(chicken_freak,       cat_food).
+has(chicken_freak,       cigarette).
+has(home_hardware,       nail).
+has(dave,                weed).
 
-has(freshco, X)         :- vegetable(X).
-has(mr_meat, X)         :- meat(X).
-has(lcbo, X)            :- alcohol(X).
-has(chicken_freak, X)   :- soft_drink(X).
-
-has(chicken_freak,         cat_food).
-has(chicken_freak,         cigarette).
-has(home_hardware,         nail).
-has(dave,                  weed).
-
-has(sink, water)        :-
+has(sink, water)      :-
     could(_, repair, sink),
     !.
 
 eat(sybil, turkey).
 
-eat(sybil, X)           :-
+eat(sybil, X)         :-
     vegetable(X).
 
-eat(kiki,  X)           :-
+eat(kiki,  X)         :-
     food(X),
     \+ eat(sybil, X),
     \+ dislike(kiki, X).
 
-eat(Who,   X)           :-
-    cat(Who), meat(X).
+eat(Cat,   X)         :-
+    cat(Cat), meat(X).
 
 drink(sybil, pepsi).
 drink(sybil, tequila).
 drink(kiki, energy_drink).
-drink(kiki, X) :- alcohol(X), \+ dislike(kiki, X).
-drink(Cat, water) :- cat(Cat).
+
+drink(kiki, X)        :-
+    alcohol(X),
+    \+ dislike(kiki, X).
+
+drink(Cat, water)     :-
+    cat(Cat).
     
 smoke(X,     weed) :- human(X).
 smoke(kiki,  cigarette).
 
-human(X)  :- person(X), \+ cat(X).    
+human(  X)            :-
+    person(X), \+ cat(X).
 
-location(X) :- store(X).
+location(X)           :-
+    store(X).
 
-exist(X) :- person(X); thing(X); location(X).
+exist(X)              :-
+    person(X) ;
+    thing(X) ;
+    location(X).
 
 provider(X, Thing) :-
-    (person(X); store(X)), has(X, Thing).
+    (person(X) ;
+     store(X)),
+    has(X, Thing).
--- infer.pl
+++ infer.pl
@@ -3,12 +3,11 @@
 start(X) :- person(X).
 
 move(Here, To) :-
-    ((person(Here),
-      appliance(To)) ;
-     (person(Here),
-      ((person(To), (like(Here,To) ;
-                     like(To,Here))) ;
-       (store(To), shop(Here,To))))).
+    (person(Here),
+     ((appliance(To));
+      (person(To), (like(Here,To) ;
+                    like(To,Here))) ;
+      store(To), shop(Here,To))).
 
 stop(Location, Target) :-
     (store(Location);
@@ -19,7 +18,6 @@ apath(Here, To)       :- apath(Here, To, _).
 apath(Here, To, Path) :-
     path(start, Here, move, To, stop, Path).
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 could(   Person, Action, Thing      )   :- could(Person, Action, Thing, _).
@@ -27,11 +25,17 @@ could(   Person, Action, Thing, Path)   :-
     a(   Person, Action, Thing      ),
     call((apath(Person,Thing,Path), !)).
 
+
+couldnt(   Person, Action, Thing      ) :- couldnt(Person, Action, Thing, _).
+couldnt(   Person, Action, Thing, Path) :-
+    \+ could(Person, Action, Thing, Path).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-a(Person,    shower, [])   :- human(Person), apath(Person, water).
-a(Person,    starve, [])   :- person(   Person   ), \+ could(    Person, eat,    _           ).
-a(Appliance, break,  [])   :- appliance(Appliance), \+ could(    _,      repair, Appliance   ).
+a(Person,    shower, [])   :- human(Person),           apath(    Person, water               ).
+a(Person,    starve, [])   :- person(   Person   ),  couldnt(    Person, eat,    _           ).
+a(Appliance, break,  [])   :- appliance(Appliance),  couldnt(    _,      repair, Appliance   ).
 
 a(Person,    chase,  X)    :- cat(      Person   ),    cat(      X),     chase(  Person, X   ).
 a(Person,    drink,  X)    :- person(   Person   ),    thing(    X),     drink(  Person, X   ).
