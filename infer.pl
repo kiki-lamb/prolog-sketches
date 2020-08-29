6c4ea2363e8c07a26e03cac2a3da87b8ff73af3e
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-29 07:39:45 -0400

    ...

--- infer.pl
+++ infer.pl
@@ -1,96 +1,94 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start(X)                               :- person(X);
-                                          appliance(X).
+start(X)                                      :- person(X);
+                                                 appliance(X).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-move(Here, To)                         :- would(Here, operate,  To);
-                                          would(Here, get_help, To);
-                                          would(Here, shop_at,  To).
+move(Here, To)                                :- would(Here, operate,  To);
+                                                 would(Here, get_help, To);
+                                                 would(Here, shop_at,  To).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-stop(Here, To)                         :- provider(Here, To).
+stop(Here, To)                                :- provider(Here, To).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ppath(Here, To)                        :- ppath(Here, To, _).
-ppath(Here, To, Path)                  :- path(start, Here, move, To, stop, Path).
+ppath(Here, To)                               :- ppath(Here, To, _).
+ppath(Here, To, Path)                         :- path(start, Here, move, To, stop, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-could(  Person, Action, Thing      )   :- could(Person, Action, Thing, _       ).
-could(  Person, Action, Thing, Path)   :- (would(Person, Action, Thing         ),
-                                           call((ppath(Person,Thing,Path),  !))).
+could(  Person, Action, Thing      )          :- could(Person, Action, Thing, _       ).
+could(  Person, Action, Thing, Path)          :- (would(Person, Action, Thing         ),
+                                                  call((ppath(Person,Thing,Path),  !))).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-could_not(Person, Action, Thing      ) :- could_not(Person, Action, Thing, _   ).
-could_not(Person, Action, Thing, Path) :- \+ could( Person, Action, Thing, Path).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-would_not(X, Action, Y) :- \+ would(X, Action, Y).
+could_not(Person, Action, Thing      )        :- could_not(Person, Action, Thing, _   ).
+could_not(Person, Action, Thing, Path)        :- \+ could( Person, Action, Thing, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-would( Start,     Action,     X          ) :- start(Start), % bind early for ordering.
-                                              wwould(Start, Action, X).
+would(    Start,     Action,     X          ) :- start(Start), % bind early for ordering.
+                                                 wwould(Start, Action, X).
 
-wwould(Person,    borrow,     Object     ) :- person(Person),
-                                              object(Object),
-                                              would(Someone, buy, Object),
-                                              ppath(Person, Someone).
+would_not(X,         Action,     Y          ) :- \+ would(X, Action, Y).
 
-wwould(Appliance, break,      Appliance  ) :- appliance(Appliance),
-                                              human(Human),
-                                              could_not(Human,repair, Appliance).
+wwould(   Person,    borrow,     Object     ) :- person(Person),
+                                                 object(Object),
+                                                 would(Someone, buy, Object),
+                                                 ppath(Person, Someone).
 
-wwould(Person,    buy,        Object     ) :- object(Object),
-                                              has(Store, Object),
-                                              would(Person, shop_at, Store).
+wwould(   Appliance, break,      Appliance  ) :- appliance(Appliance),
+                                                 human(Human),
+                                                 could_not(Human,repair, Appliance).
 
-wwould(Cat,       chase,      C2         ) :- cat(Cat),
-                                              cat(C2),
-                                              Cat \== C2.
+wwould(   Person,    buy,        Object     ) :- object(Object),
+                                                 has(Store, Object),
+                                                 would(Person, shop_at, Store).
 
-wwould(Person,    drink,      Drink      ) :- object(Drink),
-                                              drink(Person, Drink).
+wwould(   Cat,       chase,      C2         ) :- cat(Cat),
+                                                 cat(C2),
+                                                 Cat \== C2.
 
-wwould(Person,    eat,        Food       ) :- food(Food),
-                                              eat(Person, Food).
+wwould(   Person,    drink,      Drink      ) :- object(Drink),
+                                                 drink(Person, Drink).
 
-wwould(Person,    get_help,   P2         ) :- would(P2, help, Person).
+wwould(   Person,    eat,        Food       ) :- food(Food),
+                                                 eat(Person, Food).
 
-wwould(Person,    help,       P2         ) :- person(Person),
-                                              person(P2),
-                                              (like(Person,P2) ;
-                                               like(P2,Person)).
+wwould(   Person,    get_help,   P2         ) :- would(P2, help, Person).
 
-wwould(Human,     operate,    Appliance  ) :- human(Human),    
-                                              appliance(Appliance).
+wwould(   Person,    help,       P2         ) :- person(Person),
+                                                 person(P2),
+                                                 (like(Person,P2) ;
+                                                  like(P2,Person)).
 
-wwould(Person,    pet,        Cat        ) :- human(Person),
-                                              cat(Cat),
-                                              would(Person, help, Cat).
+wwould(   Human,     operate,    Appliance  ) :- human(Human),    
+                                                 appliance(Appliance).
 
-wwould(Human,     repair,     Appliance  ) :- human(Human),
-                                              appliance(Appliance),
-                                              ppath(Human, screw).
+wwould(   Person,    pet,        Cat        ) :- human(Person),
+                                                 cat(Cat),
+                                                 would(Person, help, Cat).
 
-wwould(Person,    scare_off,  Cat        ) :- person(Person),
-                                              cat(Cat),
-                                              Person \== Cat,
-                                              would_not(Person, help, Cat).
+wwould(   Human,     repair,     Appliance  ) :- human(Human),
+                                                 appliance(Appliance),
+                                                 ppath(Human, screw).
 
-wwould(Person,    smoke,      Thing      ) :- thing(Thing),
-                                              smoke(Person, Thing).
+wwould(   Person,    scare_off,  Cat        ) :- person(Person),
+                                                 cat(Cat),
+                                                 Person \== Cat,
+                                                 would_not(Person, help, Cat).
 
-wwould(Human,     shop_at,    Store      ) :- human(Human),
-                                              store(Store),
-                                              shop_at(Human, Store).
+wwould(   Person,    smoke,      Thing      ) :- thing(Thing),
+                                                 smoke(Person, Thing).
 
-wwould(Human,     shower,     Appliance  ) :- human(Human),
-                                              appliance(Appliance),
-                                              provider(Appliance, water).
+wwould(   Human,     shop_at,    Store      ) :- human(Human),
+                                                 store(Store),
+                                                 shop_at(Human, Store).
 
-wwould(Person,    starve_for, food       ) :- person(Person),
-                                              could_not(Person, eat, _).
+wwould(   Human,     shower,     Appliance  ) :- human(Human),
+                                                 appliance(Appliance),
+                                                 provider(Appliance, water).
 
-wwould(Person,    starve_for, water      ) :- person(Person),
-                                              could_not(Person, drink, water).
+wwould(   Person,    starve_for, food       ) :- person(Person),
+                                                 could_not(Person, eat, _).
+
+wwould(   Person,    starve_for, water      ) :- person(Person),
+                                                 could_not(Person, drink, water).
