10a8510d94f3f5f104931a996fd5fe794aa9fafa
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-29 08:37:53 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -5,7 +5,6 @@ path(   Start, Here, Move, To,   Stop, Path) :- call(Start, Here),
                                                 reverse([To|Tmp], Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- table search/6.
 search( Build, Here, Move, To,   Stop, Path) :- found(Build, Here, To, Stop, Path) ;
                                                 descend([Here|Build], Here, Move, To, Stop, Path).
 
