8f95d783c5ce28d0349f4ba590beabbef057394d
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 16:10:20 -0400

    ...

--- infer-2.pl
+++ infer-2.pl
@@ -12,7 +12,7 @@
 
 :- op(300, xfy, isnt).
 isnt(Thing, Class) :-
-  \+( is_a(Thing, Class)).
+   \+( is_a(Thing, Class)).
 
 :- op(300, xfy, is_a).
 is_a(Thing, Class) :-
@@ -23,105 +23,106 @@ is_a(Thing, Class) :-
    ).
 
 unique(Thing) :-
-  \+( _ is_a Thing).
+   \+( _ is_a Thing).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 combine(Left, Right, Out) :-
-  findall([ L, R ], (member(L, Left), member(R, Right)), Out).
+   findall([ L, R ], (member(L, Left), member(R, Right)), Out).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 bind_classes :-
-  is_a(Thing, Class),
-  assert_list([Class, Thing]),
-  fail.
+   is_a(Thing, Class),
+   
+   assert_list([Class, Thing]),
+   fail.
 
 bind_classes(Left, Action, Right, Out) :-
-  % format("[ Bind ~w ~w ~w ]:\n", [Left, Action, Right]),
-  findall(L, (L is_a Left),  Lefts ),
-  % format("Lefts: ~w.\n",    [Lefts]),
-  findall(R, (R is_a Right), Rights ),
-  % format("Rights: ~w.\n",   [Rights]),
-  combine([Left | Lefts], [Right | Rights], Tmp),
-  % format("Tmp: ~w.\n",   [Tmp]),
-  findall([Action, L, R], (member([L, R], Tmp)), Out).
-  % format("Tmp2: ~w.\n",   [Tmp2]),
-  
+   % format("[ Bind ~w ~w ~w ]:\n", [Left, Action, Right]),
+   findall(L, (L is_a Left),  Lefts ),
+   % format("Lefts: ~w.\n",    [Lefts]),
+   findall(R, (R is_a Right), Rights ),
+   % format("Rights: ~w.\n",   [Rights]),
+   combine([Left | Lefts], [Right | Rights], Tmp),
+   % format("Tmp: ~w.\n",   [Tmp]),
+   findall([Action, L, R], (member([L, R], Tmp)), Out).
+% format("Tmp2: ~w.\n",   [Tmp2]),
+
 bind_classes(Left, Action, Right) :-
-  bind_classes(Left, Action, Right, Out),
-  maplist(assert_list, Out).
+   bind_classes(Left, Action, Right, Out),
+   maplist(assert_list, Out).
 
 bind_actions :-
-  r(Actor, Action, Subject, _),
-  Action \== is_a,
-  % format("[[ Binding ~w... ]]\n", [Action]),
-  bind_classes(Actor, Action, Subject),
-  fail.   
+   r(Actor, Action, Subject, _),
+   Action \== is_a,
+   % format("[[ Binding ~w... ]]\n", [Action]),
+   bind_classes(Actor, Action, Subject),
+   fail.   
 
 bind_mutual_likes :-
-  r(Actor, like, Subject, _),
-  bind_classes(Subject, like, Actor),
-  fail.   
+   r(Actor, like, Subject, _),
+   bind_classes(Subject, like, Actor),
+   fail.   
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 actors(Out) :-
-  findall(Actor, ((r(Actor, _, _, _))), Tmp),
-  sort(Tmp, Out).
+   findall(Actor, ((r(Actor, _, _, _))), Tmp),
+   sort(Tmp, Out).
 
 actions(Out) :-
-  findall(Action, ((r(_, Action, _, _))), Tmp),
-  sort(Tmp, Out).
+   findall(Action, ((r(_, Action, _, _))), Tmp),
+   sort(Tmp, Out).
 
 subjects(Out) :-
-  findall(Subject, ((r(_, _, Subject, _))), Tmp),
-  sort(Tmp, Out).
+   findall(Subject, ((r(_, _, Subject, _))), Tmp),
+   sort(Tmp, Out).
 
 non_actor_subjects(Out) :-
-  actors(Actors),
-  findall(
-     Subject, ((
-                    r(_, _, Subject, _),
-                    not(member(Subject, Actors))
-                 )),
-     Tmp
-  ),
-  sort(Tmp, Out).
+   actors(Actors),
+   findall(
+      Subject, ((
+                     r(_, _, Subject, _),
+                     not(member(Subject, Actors))
+                  )),
+      Tmp
+   ),
+   sort(Tmp, Out).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 assert_list(L) :-
-  G1 =..  L,
-  %format("-=> ~w\n", [G1]),
-  assert(G1).
+   G1 =..  L,
+   %format("-=> ~w\n", [G1]),
+   assert(G1).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 setup :-
-  assertify_lines('dat2.ssv'),
-  (
+   assertify_lines('dat2.ssv'),
+   (
       actors(Actors),
       maplist(assert, Actors)
-  ),  
-  (
+   ),  
+   (
       actions(Actions),
       maplist(assert, Actions)
-  ),
-  (
+   ),
+   (
       non_actor_subjects(Subjects),
       maplist(assert, Subjects)
-  ),  
-  (
-     bind_classes
-  ;  bind_actions
-  ;  bind_mutual_likes
-  ;  true
-  ).
-  %retract(r(_,_,_,_));
-  
+   ),  
+   (
+      bind_classes
+   ;  bind_actions
+   ;  bind_mutual_likes
+   ;  true
+   ).
+%retract(r(_,_,_,_));
 
 
 
-    
-    
+
+
+
