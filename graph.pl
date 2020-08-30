e005a93b3e5cc477bc86c711019ce1323e613a11
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 14:52:26 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -14,29 +14,36 @@ recover(Here, To, Path) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-path(   Start, Here, Move, To,   Stop      ) :- path(Start, Here, Move, To, Stop, _).
-path(   Start, Here, Move, To,   Stop, Path) :- %recover(Here, To),
-                                                recover(Here, To, Path);
-                                                
-                                                call(Start, Here),
-                                                search([], Here, Move, To, Stop, Tmp),
-                                                reverse([To|Tmp], Path),
-                                                
-                                                format("... stash ~w -> ~w: ~w.\n",
-                                                      [Here, To, Path]),
+path(Start, Here, Move, To, Stop) :-
+   path(Start, Here, Move, To, Stop, _).
 
-                                                stash(Here, To, Path).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-search( Build, Here, Move, To,   Stop, Path) :- found(Build, Here, To, Stop, Path) ;
-                                                descend([Here|Build], Here, Move, To, Stop, Path).
+path(Start, Here, Move, To, Stop, Path) :-
+   %recover(Here, To),
+   recover(Here, To, Path); 
+   call(Start, Here),
+   search([], Here, Move, To, Stop, Tmp),
+   reverse([To|Tmp], Path),   
+   format("... stash ~w -> ~w: ~w.\n",
+          [Here, To, Path]),   
+   stash(Here, To, Path).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-found(  Build, Here,       Here, _,    Path) :- Path = Build. 
-found(  Build, Here,       To,   Stop, Path) :- call(Stop, Here, To),
-                                                found([Here|Build], Here, Here, Stop, Path).
+search( Build, Here, Move, To, Stop, Path) :-
+   (
+      found(Build, Here, To, Stop, Path)
+   ;  descend([Here|Build], Here, Move, To, Stop, Path)
+   ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-descend(Build, Here, Move, To,   Stop, Path) :- call(Move, Here, Next),
-                                                not(member(Next, Build)),
-                                                search(Build, Next, Move, To, Stop, Path).
+found(Build, Here, Here, _, Path) :-
+   Path = Build.
+
+found(Build, Here, To, Stop, Path) :-
+   call(Stop, Here, To),
+   found([Here|Build], Here, Here, Stop, Path).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+descend(Build, Here, Move, To,   Stop, Path) :-
+   call(Move, Here, Next),
+   not(member(Next, Build)),
+   search(Build, Next, Move, To, Stop, Path).
