841e1198c524376a44582bebc12b80fe6a6a7800
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 15:26:20 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -3,15 +3,15 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 stash(Here, To, Path) :- assert(cache(Here, To, Path)).
 
-recover(Here, To, Path) :-
-   (
-      cache(Here, To, Path)
-   ;  cache(To, Here, Tmp),
-      reverse(Tmp,Path)
-   ),   
-   format("... grab ~w -> ~w: ~w.\n",
-          [Here, To, Path]),
-   !.
+recover(Here, To, Path) :- fail.
+%   (
+%      cache(Here, To, Path)
+%   ;  cache(To, Here, Tmp),
+%      reverse(Tmp,Path)
+%   ),
+%    % format("... grab ~w -> ~w: ~w.\n",
+%           % [Here, To, Path]),
+%   !.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -20,13 +20,13 @@ path(Start, Here, Move, To, Stop) :-
 
 path(Start, Here, Move, To, Stop, Path) :-
    (
-      %recover(Here, To),
+      % recover(Here, To),
       recover(Here, To, Path)
    ;  call(Start, Here),
       search([], Here, Move, To, Stop, Tmp),
       reverse([To|Tmp], Path),   
-      format("... stash ~w -> ~w: ~w.\n",
-             [Here, To, Path]),   
+      % format("... stash ~w -> ~w: ~w.\n",
+             % [Here, To, Path]),   
       stash(Here, To, Path)
    ).
 
