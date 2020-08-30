16884428b14c595c6e2d706f853f18e89c998065
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 15:02:37 -0400

    ...

--- graph.pl
+++ graph.pl
@@ -4,13 +4,14 @@
 stash(Here, To, Path) :- assert(cache(Here, To, Path)).
 
 recover(Here, To, Path) :-
-   ((cache(Here, To, Path) ;
-     (cache(To, Here, Tmp),
-      reverse(Tmp,Path))),
-    
-    format("... grab ~w -> ~w: ~w.\n",
-           [Here, To, Path]),    
-    !).
+   (
+      cache(Here, To, Path)
+   ;  cache(To, Here, Tmp),
+      reverse(Tmp,Path)
+   ),   
+   format("... grab ~w -> ~w: ~w.\n",
+          [Here, To, Path]),
+   !.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -20,8 +21,8 @@ path(Start, Here, Move, To, Stop) :-
 path(Start, Here, Move, To, Stop, Path) :-
    (
       %recover(Here, To),
-      recover(Here, To, Path); 
-      call(Start, Here),
+      recover(Here, To, Path)
+   ;  call(Start, Here),
       search([], Here, Move, To, Stop, Tmp),
       reverse([To|Tmp], Path),   
       format("... stash ~w -> ~w: ~w.\n",
