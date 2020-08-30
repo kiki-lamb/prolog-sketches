8bed2fe081eeafc4ea5bbce32df56ae9f25c604d
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 15:20:58 -0400

    ...

--- main.pl
+++ main.pl
@@ -3,26 +3,26 @@
 :- consult("infer-2.pl").
 :- consult("infer.pl").
 
-% possible        :- possible(_,_,_).
-% possible(W,A,T) :- could(W,A,T),
-%                    format("~w could ~w ~w.\n",
-%                           [W, A, T]),
-%                    fail.
+possible        :- possible(_,_,_).
+possible(W,A,T) :- could(W,A,T),
+                   format("~w could ~w ~w.\n",
+                          [W, A, T]),
+                   fail.
 
-% possible_paths          :- possible_paths(_,_,_,_).
-% possible_paths(P)       :- possible_paths(_,_,_,P).
-% possible_paths(W,A,T,P) :-
-%     could(W,A,T,P),
-%     format("~w could ~w ~w: ~w.\n",
-%            [W, A, T, P]),
-%     fail.
+possible_paths          :- possible_paths(_,_,_,_).
+possible_paths(P)       :- possible_paths(_,_,_,P).
+possible_paths(W,A,T,P) :-
+    could(W,A,T,P),
+    format("~w could ~w ~w: ~w.\n",
+           [W, A, T, P]),
+    fail.
 
-% desires        :- desires(_,_,_).
-% desires(P,A,X) :- would(P,A,X),
-%                   format("~w would ~w ~w.\n", [P,A,X]),
-%                   fail.  
+desires        :- desires(_,_,_).
+desires(P,A,X) :- would(P,A,X),
+                  format("~w would ~w ~w.\n", [P,A,X]),
+                  fail.  
 
-% ppaths         :- ppaths(_,_,_).
-% ppaths(W,T,P)  :- ppath(W,T,P),
-%                   format("~w -> ~w: ~w\n", [W,T,P]),
-%                   fail.
+ppaths         :- ppaths(_,_,_).
+ppaths(W,T,P)  :- ppath(W,T,P),
+                  format("~w -> ~w: ~w\n", [W,T,P]),
+                  fail.
