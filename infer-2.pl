efb58df14db0c20abefd2784ebeadb0d349ba4c9
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 11:53:16 -0400

    ...

--- infer-2.pl
+++ infer-2.pl
@@ -40,72 +40,51 @@ loop_as :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 combine(Left, Right, Out) :-
   bagof([ L, R ], (member(L, Left), member(R, Right)), Out).
 
-bind_list(Left, Right, Out) :-
+bind_classes(Left, Action, Right) :-
   bagof(L, (is_a(L, Left )), Lefts ),
-  bagof(R, (is_a(R, Right)), Rights),
-  combine([Left | Lefts], [Right | Rights], Out).
-
-bind_to(Class, Action, Subject) :-
-  format("[~w ~w ~w]\n", [Class, Action, Subject]),
-  dddeclarify(Action, Class, Subject);
-  bagof(SubjectSubClass, (is_a(SubjectSubClass, Subject)), SubjectSubClasses),
-  maplist(dddeclarify(Action, Class), SubjectSubClasses),
-
-  bagof(SubClass,        (is_a(SubClass,        Class)),   SubClasses),
+  format("Lefts: ~w.\n",    [Lefts]),
   
-  format("~w actors:  ~w.\n", [ Class, SubClasses ]),
-  format("~w targets: ~w.\n", [ Subject, SubjectSubClasses ]),
+  bagof(R, (is_a(R, Right)), Rights ),
+  format("Rights: ~w.\n",   [Rights]),
   
-  maplist(derp(Action, SubClasses), SubjectSubClasses).
+  combine([Left | Lefts], [Right | Rights], Tmp),
+  format("Tmp: ~w.\n",   [Tmp]),
 
-
-
-magic_loop(Action) :- 
-  r(Class, Action, Subject, _),
-  bind_to(Class, Action, Subject).
+  bagof([Action, L, R], (member([L, R], Tmp)), Tmp2),
+  format("Tmp2: ~w.\n",   [Tmp2]),
   
-%  format("[ ~w ~w ~w ]\n", [Class, Action, Subject]),
-%  declarify(Action, Class, Subject),
-%
-%  is_a(Thing, Class),
-%  format("[ ~w ~w ~w ]\n", [Thing, Action, Subject]),
-%  declarify(Action, Thing, Subject). 
-
-loop_binary(Action) :-
-  r(Thing, Action, Thing2, _),
-  declarify(Action, Thing, Thing2),
-  fail.
-
-loop_reflex(Action) :-
-  r(Thing, Action, Thing2, _),
-  (declarify(Action, Thing, Thing2);
-   declarify(Action, Thing2, Thing)).
+  maplist((declarify(Action)), Tmp2, _).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-declarify(Action, Thing) :-
-  G1 =.. [ Action, Thing ],
+declarify([Action, Thing, Subject]) :-
+  G1 =..  [Action, Thing, Subject],
   aassertz(Action, G1).
 
 declarify(Action, Thing, Subject) :-
   G1 =.. [ Action, Thing, Subject ],
   aassertz(Action, G1).
 
+declarify(Action, Thing) :-
+  G1 =.. [ Action, Thing ],
+  aassertz(Action, G1).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 aassertz(Action, G1) :-
   assertz(G1),
   format("=+~w> ~w.\n",
-         [Action, G1]),
-  fail.
+         [Action, G1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 setup :-
   clean_up,
   assertify_lines('dat2.ssv'),
-%  loop_as;
+  loop_as;
 %  loop_reflex(like);
 %  magic_loop(eat);
 %  magic_loop(drink);
