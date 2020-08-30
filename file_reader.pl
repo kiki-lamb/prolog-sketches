268c770e285eefb8c68981ddda80e8a02a32c42f
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 14:58:00 -0400

    rm old

--- file_reader.pl
+++ file_reader.pl
@@ -1,39 +1,56 @@
 :- use_module(library(pio)).
 
-lliness([])                              --> call(eos), !.
-lliness([CLine|Lliness])                 --> cline(CLine), lliness(Lliness).
+lliness([]) -->
+   call(eos), !.
+
+lliness([CLine|Lliness]) -->
+   cline(CLine),
+   lliness(Lliness).
 
 eos([], []).
 
-cline([])                                --> ( ".\n" ; call(eos) ), !.
-cline([L|Ls])                            --> [L], cline(Ls).
-clines(Ls, File)                          :- phrase_from_file(lliness(Ls), File).
+cline([]) -->
+   (".\n" ; call(eos)), !.
 
-collect(Build, Out)                       :- Out = Build.
-collect(Using, In, Out)                   :-
-  G =.. [ Using, In, [], Tmp ],
-  call(G),
-  reverse(Tmp, Out).
+cline([L|Ls]) -->
+   [L], cline(Ls).
 
-assertify_lines(File)                     :-
-  clines(Ls, File),
-  assertify_lines(Ls, _).
+clines(Ls, File) :-
+   phrase_from_file(lliness(Ls), File).
 
-assertify_lines(In, Out)                  :-
-  retractall(r(_,_,_,_)),
-  collect(aasertify_lines, In, Out).
+collect(Build, Out) :-
+   Out = Build.
 
-aasertify_lines([], Build, Out)           :- collect(Build, Out), !.
+collect(Using, In, Out) :-
+   G =.. [ Using, In, [], Tmp ],
+   call(G),
+   reverse(Tmp, Out).
+
+assertify_lines(File) :-
+   clines(Ls, File),
+   assertify_lines(Ls, _).
+
+assertify_lines(In, Out) :-
+   retractall(r(_,_,_,_)),
+   collect(aasertify_lines, In, Out).
+
+aasertify_lines([], Build, Out) :-
+   collect(Build, Out), !.
+
 aasertify_lines([Line|Lines], Build, Out) :-
-  split_string(Line, " ", " ", Words),
-  atomize(Words, [A1, A2, A3 | Atoms]),
-  Term =.. [r, A1, A2, A3, Atoms],
-  assertz(Term),
-  aasertify_lines(Lines, Build, Out).
+   split_string(Line, " ", " ", Words),
+   atomize(Words, [A1, A2, A3 | Atoms]),
+   Term =.. [r, A1, A2, A3, Atoms],
+   assertz(Term),
+   aasertify_lines(Lines, Build, Out).
 
-atomize(In, Out)                          :- collect(aatomize, In, Out).
-aatomize([], Build, Out)                  :- collect(Build, Out), !.
-aatomize([In|Ins], Build, Out)            :-
+atomize(In, Out) :-
+   collect(aatomize, In, Out).
+
+aatomize([], Build, Out) :-
+   collect(Build, Out), !.
+
+aatomize([In|Ins], Build, Out) :-
   atom_codes(Atomic, In),
   aatomize(Ins, [Atomic|Build], Out).
 
