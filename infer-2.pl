9e7e590b93e0d1bb7dd537cb751e53493fa46856
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-30 08:22:33 -0400

    no modify static pred

--- infer-2.pl
+++ infer-2.pl
@@ -36,69 +36,42 @@ manifest(Term) :-
   format("    Manifest nothing ~w.\n",
          [Term]).  
 
-
-
 a(  Obj,             SoughtType) :-
   r(Obj,         a,  SoughtType, _);
   r(Obj,         a,  ActualType, _),
   a(ActualType,      SoughtType   ).
 
-loop_as     :- a(Obj, Thing),
-               G1 =.. [ Thing, Obj ],
-               format("=a>       ~w.\n",
-                      [G1]),                 
-               assertz(G1),
-               fail.
+loop_as :-
+  a(Obj, Thing),
+  declarify(Thing, Obj).
 
-loop_likes  :- r(Thing, like, Thing2, _),
-               G1 =.. [ like, Thing,  Thing2 ],
-               G2 =.. [ like, Thing2, Thing  ],
-               format("=like>    ~w.\n",
-                      [G1]),                 
-               assertz(G1),
-               format("=like>    ~w.\n",
-                      [G2]),                 
-               assertz(G2),
-               fail.
+declarify(Type, Thing) :-
+  G1 =.. [ Type, Thing ],
+  aassertz(Type, G1).
 
-loop_shop_at :- r(Thing, shop_at, Thing2, _),
-                G1 =.. [ shop_at, Thing,  Thing2 ],
-                format("=shop_at> ~w.\n",
-                       [G1]),                 
-                assertz(G1),
-                fail.
+declarify(Type, Thing, Thing2) :-
+  G1 =.. [ Type, Thing, Thing2 ],
+  aassertz(Type, G1).
 
+aassertz(Type, G1) :-
+  assertz(G1),
+  format("=+~w> ~w.\n",
+         [Type, G1]),
+  fail.
 
+loop_binary(Action) :- r(Thing, Action, Thing2, _),
+                       declarify(Action, Thing, Thing2).
 
+loop_reflexive(Action) :- r(Thing, Action, Thing2, _),
+                          (declarify(Action, Thing, Thing2);
+                           declarify(Action, Thing2, Thing)).
 
-
-loop_binary(Type) :- r(Thing, Type, Thing2, _),
-                     G1 =.. [ Type, Thing,  Thing2 ],
-                     format("=+~w> ~w.\n",
-                            [Type, G1]),                 
-                     assertz(G1),
-                     fail.
-
-
-loop_dislike :- r(Thing, dislike, Thing2, _),
-                G1 =.. [ dislike, Thing,  Thing2 ],
-                format("=dislike> ~w.\n",
-                       [G1]),                 
-                assertz(G1),
-                fail.
-
-loop_rs     :- r(Thing, Verb, Noun, Options),
-               format("\n+=> r(~w, ~w, ~w, ~w.\n",
-                      [Thing, Verb, Noun, Options]),
-               manifest((r(Thing, Verb, Noun, Options))),
-               fail.
-
-setup       :- clean_up,
-               assertify_lines('dat2.ssv'),
-               loop_as;
-               loop_likes;
-               loop_binary(shop_at);
-               loop_binary(dislike);
-               true.
+setup :- clean_up,
+         assertify_lines('dat2.ssv'),
+         loop_as;
+         loop_reflexive(like);
+         loop_binary(shop_at);
+         loop_binary(dislike);
+         true.
 
 
