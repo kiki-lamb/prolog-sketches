4059e21942c169dcb558acb17498c573034e4d83
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-31 14:28:47 -0400

    ..

--- graph.pl
+++ graph.pl
@@ -1,5 +1,3 @@
-:- dynamic cache/3.         
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 move( Here, Here ) :- fail.
@@ -25,14 +23,28 @@ path(Here, Here, [Here]) :-
 %      format("   .> path(~w, ~w, ~w)\n",
 %             [Here, Here, [Here]]).
 
+% path(Here, , Path) :-
+
+:- dynamic cached_path/3. 
+
+%cached_path(_,_,_) :- false.
+
 path(Here, There, Path) :-
    (
+      cached_path(Here, There, Path),
+      format(" ..O> path(~w, ~w, ~w)\n",
+             [Here, There, Path])
+   ;
+      
       start(Here),
       start(There),
       search([], Here, There, Tmp),
       reverse([There|Tmp], Path),
+      reverse(Path, RevPath),
       format(" .oO> path(~w, ~w, ~w)\n",
-             [Here, There, Path])
+             [Here, There, Path]),
+      assertz(cached_path(Here, There, Path   )),
+      assertz(cached_path(There, Here, RevPath))
    ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
