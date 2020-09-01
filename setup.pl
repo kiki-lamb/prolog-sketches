c83da13bb0bda2a32da637c7b85fb5adfa24a5e4
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-08-31 21:18:42 -0400

    inefficient retrieval

--- setup.pl
+++ setup.pl
@@ -23,21 +23,16 @@ combine(Left, Right, Out) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 bind_classes :-
-%   concrete(Thing),
    memberof(Thing, Class),
    logged_assert_list([Class, Thing]),
    fail.
 
 bind_classes(Left, Action, Right, Out) :-
-   % format("[ Bind ~w ~w ~w ]:\n", [Left, Action, Right]),
    findall(L, (memberof(L, Left)),  Lefts ),
-   % format("Lefts: ~w.\n",    [Lefts]),
    findall(R, (memberof(R, Right)), Rights ),
-   % format("Rights: ~w.\n",   [Rights]),
    combine([Left | Lefts], [Right | Rights], Tmp),
-   % format("Tmp: ~w.\n",   [Tmp]),
    findall([Action, L, R], (member([L, R], Tmp)), Out).
-% format("Tmp2: ~w.\n",   [Tmp2]),
+
 
 bind_classes(Left, Action, Right) :-
    bind_classes(Left, Action, Right, Out),
@@ -46,7 +41,6 @@ bind_classes(Left, Action, Right) :-
 bind_actions :-
    r(Actor, Action, Subject, _),
    Action \== isa,
-   % format("[[ Binding ~w... ]]\n", [Action]),
    bind_classes(Actor, Action, Subject),
    fail.   
 
@@ -131,6 +125,8 @@ setup :-
       bind_actions      
    ;  format("[[Setup]] Binding mutual Likes.\n",[]),
       bind_mutual_likes
+   ;  format("[[Setup]] Charting paths...\n",[]),
+      stash_paths
    ;  format("[[Setup]] Complete.\n",[]),
       true
    ), !.
