b3296b322304f1f931a021ecdff7658e713acdc0
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-09-02 18:22:35 -0400

    ...

--- main.pl
+++ main.pl
@@ -17,6 +17,8 @@ main :-
    nl.
 
 
+%-----------------------------------------------------------
+
 possible        :- possible(_,_,_).
 possible(W,A,T) :-
    could(W,A,T),
@@ -24,6 +26,8 @@ possible(W,A,T) :-
    format("~w could ~w ~w.\n",
           [W, SA, T]),
    fail.
+
+%-----------------------------------------------------------
 
 possible_paths          :- possible_paths(_,_,_,_).
 possible_paths(P)       :- possible_paths(_,_,_,P).
@@ -34,16 +38,22 @@ possible_paths(W,A,T,P) :-
            [W, SA, T, P]),
    fail.
 
+%-----------------------------------------------------------
+
 looperp(Func) :-
    call(Func, Something),
    format("  -  ~w.\n", [Something]),
    fail.
+
+%-----------------------------------------------------------
 
 desires        :- desires(_,_,_).
 desires(P,A,X) :-
    would(P,A,X),
    format("~w would ~w ~w.\n", [P,A,X]),
    fail.
+
+%-----------------------------------------------------------
 
 singular(In, Out) :-
    re_replace("s$", "", In, Out).
