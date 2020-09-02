dc2a9941d2262c0c9b8920233a620b045863f1b2
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-09-02 07:23:57 -0400

    simplify

--- mini.pl
+++ mini.pl
@@ -4,13 +4,14 @@ likes(kiki, sybil).
 likes(boson, kiki).
 likes(kiki, boson).
 
+likes(boson, higgy).
+likes(higgy, boson).
 
 search(Here, Here, [Here|Path], Path).
 
 search(Here, There, Path, Build) :-
    likes(There, Next),
    not(member(Next, [There|Build])),
-   
    search(Here, Next, Path, [There|Build]).
 
 
