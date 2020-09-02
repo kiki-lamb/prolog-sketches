35113953b3887788e480daa4960c7af300c054fe
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-09-02 04:36:12 -0400

    ...

--- file_reader.pl
+++ file_reader.pl
@@ -4,29 +4,35 @@
 % Preds
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-load_atomized_lines_from_file(TagAs, File) :-
-   load_atomized_lines_from_file(TagAs, File, _).
+load_atomized_lines_from_file(Tag, File) :-
+   load_atomized_lines_from_file(Tag, File, _).
 
-load_atomized_lines_from_file(TagAs, File, Out) :- 
+load_atomized_lines_from_file(Tag, File, Out) :- 
    retractall(r(_,_,_,_)),
    phrase_from_file(lines(In), File),
-   atomize_lines(TagAs, In, TmpOut),
+   atomize_lines(Tag, In, TmpOut),
    maplist(assert, TmpOut, TmpOut2),
    reverse(TmpOut2, Out).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-atomize_lines(TagAs, Lines, Out) :-
-   atomize_lines([], TagAs, Lines, Out).
+atomize_lines(Tag, Lines, Out) :-
+   atomize_lines([], Tag, Lines, Out).
 
 atomize_lines(Build, _, [], Out) :-
    Out = Build, !.
 
-atomize_lines(Build, TagAs, [Line|Lines], Out) :-
+atomize_lines(Build, Tag, [Line|Lines], Out) :-
    split_string(Line, " ", " ", Words),
+
    atomize(Words, [A1, A2, A3 | Atoms]),
-   G1 =.. [ TagAs, A1, A2, A3, Atoms ],
-   atomize_lines([G1|Build], TagAs, Lines, Out).
+   G1 =.. [ Tag, A1, A2, A3, Atoms ],
+   
+   atomize_lines([G1|Build], Tag, Lines, Out).
+
+f_atomize(Tag, Words, [A1, A2, A3 | Atoms], G1),
+   G1 =.. [ Tag, A1, A2, A3, Atoms ].
+   
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
