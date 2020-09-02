fe230962d9694584a966e53004e86496758672ab
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-09-02 08:38:52 -0400

    clean

--- mini.pl
+++ mini.pl
@@ -1,3 +1,6 @@
+:- initialization(main).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 likes(sybil, kiki).
 likes(kiki, sybil).
 
@@ -7,6 +10,11 @@ likes(kiki, boson).
 likes(boson, higgy).
 likes(higgy, boson).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+search(Here, There, Path) :-
+   search(Here, There, Path, []).
+
 search(Here, Here, Path, Build) :-
    reverse([Here|Build], Path).
 
@@ -14,3 +22,10 @@ search(Here, There, Path, Build) :-
    likes(Here, Next),
    not(member(Next, [Here|Build])),
    search(Next, There, Path, [Here|Build]).
+
+main :-
+   search(higgy, sybil, Path),
+   format("~w.\n", [Path]),
+   search(sybil, higgy, Path2),
+   format("~w.\n", [Path2]),
+   halt.
