d19abfb4f13f370b5dd723d2a7fddaf95dc0c3f5
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-09-06 00:36:53 -0400

    words kind of working?

---
+++ dcg.pl
@@ -0,0 +1,44 @@
+% :- initialization(main).
+:- use_module(library(pio)).
+:- use_module(library(dcg/basics)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Grammar.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+lines([])     --> end_of_sequence, !.
+lines([L|Ls]) --> line(L),
+                  lines(Ls).
+
+line([])      --> "\n", !.
+line([])      --> end_of_sequence, !.
+line([L|Ls])  --> word(L), line(Ls).
+
+
+words([])     --> end_of_sequence, !.
+words([L|Ls]) --> word(L),
+
+                  lines(Ls).
+word([])      --> " ", !.
+word([])      --> ".", !.
+word([])      --> end_of_sequence, !.
+word([L|Ls])  --> [L], word(Ls).
+
+
+end_of_sequence([], []).
+
+ml(L, L2) :- maplist(string_codes, L, L2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main :-
+   phrase_from_file(lines(Lines), "tiny.ssv"),
+   nl,
+   write(Lines),
+   nl, nl,
+   maplist(ml, Lines2, Lines),
+   nl,
+   write(Lines2),
+   nl, nl.
+
